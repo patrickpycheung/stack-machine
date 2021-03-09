3d8f4f80ae8402fb8134f4a2322707f83139b6db
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-09 21:10:18 +1100

    Create initial Spring Boot skeleton.

---
+++ src/main/java/com/somecompany/StackMachineApplication.java
@@ -0,0 +1,13 @@
+package com.somecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackMachineApplication.class, args);
+	}
+
+}
