f699650b818b519bcfa41803863caee20bfad19f
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-10 23:01:21 +1100

    Amend handling of UNDO command in console.

--- src/main/java/com/somecompany/StackMachineApplication.java
+++ src/main/java/com/somecompany/StackMachineApplication.java
@@ -128,7 +128,18 @@ public class StackMachineApplication implements CommandLineRunner {
 						} else if (command.equals(Command.UNDO.name())) {
 							// UNDO command
 
-							stackMachineService.undo();
+							String result = stackMachineService.undo();
+
+							if (result.equals("EMPTY")) {
+								// Stack is empty
+
+								System.out.println("There are now no elements in the stack.");
+							} else {
+								// Stack is not empty
+
+								// Print top element
+								System.out.println("Top element: " + result);
+							}
 						} else if (command.equals(Command.PRINT.name())) {
 							// PRINT command
 
